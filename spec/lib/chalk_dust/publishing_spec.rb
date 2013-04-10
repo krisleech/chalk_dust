1a24d2929e965f5690c56582c9bf921e448694d2
Kris Leech <kris.leech@gmail.com>
2013-04-10 18:50:58 +0100

    rejig spec blocks a little

--- spec/lib/chalk_dust/publishing_spec.rb
+++ spec/lib/chalk_dust/publishing_spec.rb
@@ -6,30 +6,32 @@ describe 'publishing' do
     ChalkDust::ActivityItem.delete_all
   end
 
-  it '.publish_event creates an event for every subscriber' do
-    kris    = User.create!
-    lindsey = User.create!
-    hallie  = User.create!
-    post    = Post.create!
+  describe '.publish_event' do
+    it 'creates an event for every subscriber' do
+      kris    = User.create!
+      lindsey = User.create!
+      hallie  = User.create!
+      post    = Post.create!
 
-    ChalkDust::Connection.create(:subscriber => kris,    :publisher => post)
-    ChalkDust::Connection.create(:subscriber => lindsey, :publisher => post)
+      ChalkDust::Connection.create(:subscriber => kris,    :publisher => post)
+      ChalkDust::Connection.create(:subscriber => lindsey, :publisher => post)
 
-    activity_items = ChalkDust.publish_event(kris, 'editted', post)
+      activity_items = ChalkDust.publish_event(kris, 'editted', post)
 
-    activity_items.size.should == 2
+      activity_items.size.should == 2
 
-    activity_item = activity_items.first
-    activity_item.performer.should == kris
-    activity_item.event.should == 'editted'
-    activity_item.target.should == post
-    activity_item.owner.should == kris
+      activity_item = activity_items.first
+      activity_item.performer.should == kris
+      activity_item.event.should == 'editted'
+      activity_item.target.should == post
+      activity_item.owner.should == kris
 
-    activity_item = activity_items.last
-    activity_item.performer.should == kris
-    activity_item.event.should == 'editted'
-    activity_item.target.should == post
-    activity_item.owner.should == lindsey
+      activity_item = activity_items.last
+      activity_item.performer.should == kris
+      activity_item.event.should == 'editted'
+      activity_item.target.should == post
+      activity_item.owner.should == lindsey
+    end
   end
 end
 
