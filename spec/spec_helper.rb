a4d47749fa9f5459bb94a1b57d920408d3ee2ab9
Kris Leech <kris.leech@gmail.com>
2013-05-23 17:05:18 +0100

    reindent [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ puts "Using ActiveRecord #{ActiveRecord::VERSION::STRING}"
 adapter = RUBY_PLATFORM == "java" ? 'jdbcsqlite3' : 'sqlite3'
 
 ActiveRecord::Base.establish_connection(:adapter => adapter,
-                                       :database => File.dirname(__FILE__) + "/db.sqlite3")
+                                        :database => File.dirname(__FILE__) + "/db.sqlite3")
 
 load File.dirname(__FILE__) + '/support/schema.rb'
 
